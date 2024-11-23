02c73d3ce39b72c7804ad72944dd56b612ca8ec6
Martin <martin.guerout@gmail.com>
2024-11-23 17:21:10 +0100

    merge de la branche main vers COBRA-implemetation

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import random
-def create_account():
-    username=input("Entrez votre nom d'utilisateur:")
-    password=input("Entrez votre mot de passe:")
-    with open('users.txt', 'a') as file:
-        file.write(username + ' ' + password + '\n')
-    print("Votre compte a été créé avec succès!")
-=======
 import argparse
 import re
 from client import *
@@ -23,33 +14,7 @@ def initializeParser():
     args = parser.parse_args()
     return args
 
-<<<<<<< HEAD
-#Cette fonction permet de générer une clé de session aléatoire dans un bitarray
-def genSessionKey():
-    sessionKey=''
-    while True:
-        keySize=int(input("Quelle taille de clé voulez-vous générer? (128, 192 ou 256): "))
-        if keySize == 128:
-            sessionKey=format(random.getrandbits(128), '0128b')
-            break
-        elif keySize == 192:
-            sessionKey=format(random.getrandbits(192), '0192b')
-            break
-        elif keySize == 256:
-            sessionKey=format(random.getrandbits(256), '0256b')
-            break
-        else:
-            print("Wrong key size")
-    return sessionKey
 
-def main():
-    ans=True
-    while ans:
-        print("Bonjour ô maître T ! Que souhaitez-vous faire aujourd'hui?")
-        print("1. Créer votre compte")
-        print("2. Vous connecter")
-        print("3. Quitter")
-=======
 def errorManagement(errorCode):
     switcher = {
         0: "Le programme ne peut pas être à la fois client et serveur.",
