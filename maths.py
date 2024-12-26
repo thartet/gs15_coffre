84eb984084852fcb0e92e0861984f953c7859e18
thartet <nolwenn.taton@utt.fr>
2024-12-26 14:54:26 +0100

    RSA algorithm

---
+++ maths.py
@@ -0,0 +1,66 @@
+import random
+
+def euclide(a, b):
+    """
+    Application of the Euclide theorem
+    a, b : input numbers
+    returns the greatest common divisor of a and b
+    """
+    if b == 0:
+        return (a, 1, 0)
+    else:
+        (d, u, v) = euclide(b, a % b)
+        return (d, v, u - (a // b) * v)
+
+def bezout(a, b):
+    """
+    Application of the Bezout theorem
+    a, b : input numbers
+    returns the inverse of a modulo b
+    """
+    (d, u, v) = euclide(a, b)
+    return u
+
+def is_prime(n,k=10):
+    """
+    Miller-Rabin primality test
+    n : input number
+    k : number of iterations
+    returns True if the number is prime
+    returns False if the number is non prime
+    """
+
+    if n < 2:
+        return False
+    
+    if n != 2 and n % 2 == 0:
+        return False
+    
+    s = n - 1
+
+    while s % 2 == 0:
+        s = s // 2
+    
+    for i in range(k):
+        a = random.randrange( n - 1 ) + 1
+        temp = s
+        mod = pow(a, temp, n)
+        while temp != n - 1 and mod != 1 and mod != n - 1:
+            mod = (mod * mod) % n
+            temp *= 2
+            if mod != n - 1 and temp % 2 == 0:
+                return False
+        return True
+
+
+def random_prime(bits) :
+    """
+    Generate a random prime number
+    bits : The size in bits of the number to generate
+    returns the number generated
+    """
+    while True : 
+        rnumber = random.randrange(2**(bits-1), 2**(bits))
+        if is_prime(rnumber):
+            return rnumber
+
---
+++ rsa.py
@@ -0,0 +1,52 @@
+import maths
+
+def RSA ():
+    """
+    RSA algorithm
+    returns the public and private keys
+    """
+    p = maths.random_prime(512)
+    q = maths.random_prime(512)
+    n = p * q
+    phi_n = (p - 1) * (q - 1)
+    e = 65537
+    d = maths.bezout(e, phi_n)
+    d = d % phi_n
+
+    print("p :", p)
+    print("q :", q)
+    print("n :", n)
+    print("d : ", d)
+    print("Public key : ", (e, n))
+    print("Private key :", (d, n))
+
+    return ((e, n), (d, n))
+    
+def encrypt (m, pk):
+    """
+    Encrypt a message
+    m : message to encrypt
+    pk : public key
+    returns the encrypted message
+    """
+    return pow(m, pk[0], pk[1])
+
+def decrypt (c, sk):
+    """
+    Decrypt a message
+    c : message to decrypt
+    sk : private key
+    returns the decrypted message
+    """
+    return pow(c, sk[0], sk[1])
+
+
+def main():
+    pk, sk = RSA()
+    result = encrypt("test",pk)
+    print("The encrypted message is :", result)
+    print("The decrypted message is : ", decrypt(result, sk))
+
+
+if __name__ == "__main__":
+    main()
